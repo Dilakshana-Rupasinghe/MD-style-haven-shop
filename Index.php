5265fb166d82d2ef6c797ad6b72d128801da56d7
Dilakshana-Rupasinghe <153206022+Dilakshana-Rupasinghe@users.noreply.github.com>
2024-09-10 06:17:00 +0530

    footer section

--- Index.php
+++ Index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <LInk rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="home.css">
     <title>MD-Style Haven shop/online shoping-Home page</title>
 </head>
 
@@ -50,7 +50,7 @@ include('includes/navbar.php');
     <!-- Product table -->
 
     <div class="container ">
-        <div class="all-books my-3 ">
+        <div class="all-product my-3 ">
             <h2> NEW ARRIVALS  </h2>
             <hr>
             <table class="all-book-tbl" >
@@ -139,83 +139,10 @@ include('includes/navbar.php');
     </div>
 
     <!-- footer section -->
-    <div>
-        <footer class="footer-section">
-            <div class="container ">
-                <div class="row mt-2 pt-4">
-                    <div class="col-3 ">
-                        <h4>Who we are</h4>
-                        <p>
-                            Junky Books was started in 2024 with the vision of buying
-                            books online in a wide range of digital formats.
-                        </p>
-                    </div>
-                    <div class="col-2 mx-2 ">
-                        <h4 class="mx-3 ">Quick Links</h4>
-                        <ul class="footer-links">
-                            <li><a href="Index.html">Home</a></li>
-                            <li><a href="#">FAQ</a></li>
-                            <li><a href="#">About Us</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-4">
-                        <h4 class="mx-3 ">Contact</h4>
-                        <ul class="footer-links">
-                            <li>For Product Advice : +94 70 120 1347</li>
-                            <li> For Delivery Details : +94 11 234 456</li>
-                            <li> Email : <a href="#" class="email">mdstylehaven@gmail.com</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-2">
-                        <h4> Location </h4>
-                        <address>
-                            No 01,<br>
-                            MD-Styele Haven shop,<br>
-                            Rathnapura Road,<br>
-                            Avissawella.
-                        </address>
-                    </div>
-                </div>
-                <hr>
-                <div class="container ">
-                    <div class="row mt-1 ">
-                        <div class="col-6 mb-3  ">
-                            <h4> Shoicial links</h4>
-                            <ul class="social-links">
-                                <li>
-                                    <a href="#" class="facebook"> <img src="icons/facebook-circle-fill.svg" alt=""
-                                            style="width: 38px; padding-bottom: 5px;"> </a>
-                                </li>
-                                <li>
-                                    <a href="#" class="whatsapp"> <img src="icons/whatsapp-line.svg" alt=""
-                                            style="width: 38px; padding-bottom: 5px;"></a>
-                                </li>
-                                <li>
-                                    <a href="#" class="instergrame"> <img src="icons/instagram-fill.svg" alt="no image"
-                                            style="width: 38px; padding-bottom: 5px;"></a>
-                                </li>
-                                <li>
-                                    <a href="#" class="tiktok"> <img src="icons/tiktok-line.svg" alt=""
-                                            style="width: 38px; padding-bottom: 5px;"> </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </footer>
-
-        <footer class="copyr">
-            <div class="container ">
-                <div class="row col-12 pt-3 ">
-                    <p class="copy-right">Copyright &COPY; 2024 MD-Style Haven SHOP | Develop by - <a href="#"> Malindu </a> </p>
-                </div>
-            </div>
-        </footer>
-    </div>
-
+     <?php 
+        include('includes/footer.php')
+     ?>
+    <!-- footer end -->
 </body>
 
 <script>
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg bg-body-secondary  sticky-top">
     <div class="container-fluid">
         <a class="navigation-brand ms-2 me-auto" href="Index.php" >
-            <img src="images/logo .jpg" alt="logo" style="width: 100px;" class="logo">
+            <img src="images/logo .jpg" alt="logo" style="height: 80px; width: 110px;" class="logo">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -37,7 +37,7 @@
                     <a class="position-relative me-1" href="#"><img src="icons/cart-2-line.svg" style="width: 32px;">
                 </li>
                 <li class="nav-item">
-                    <a  class="nav-link "  href="#"><img src="icons/profile-line.svg" style="width: 32px;"></a>
+                    <a  href="#"><img src="icons/profile-line.svg" style="width: 32px;"></a>
                 </li>
             </ul>
         </DIV>
