f80d9c778f43ebffc85cbb3c7d6851c83e235fdc
Dilakshana-Rupasinghe <153206022+Dilakshana-Rupasinghe@users.noreply.github.com>
2024-09-13 13:20:22 +0530

    create admin home css

--- admin/home pages/admin-home.php
+++ admin/home pages/admin-home.php
@@ -5,11 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin dashboard</title>
-     <!-- Bootstrap CSS link -->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Bootstrap CSS link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- Google Material icons CSS link -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="../../css/home.css">
+    <link rel="stylesheet" href="../../css/back-home.css">
+    
 </head>
 
 <body>
@@ -21,27 +23,35 @@
 
     <div class="container">
         <!-- menu section start -->
-        <div class="menu">
-
-        </div>
+        <aside>
+            <h1>Manu</h1>
+        </aside>
         <!-- menu section end -->
 
         <!-- main section start -->
-        <div class="main">
-
-        </div>
+        <main>
+            <h1>Main</h1>
+        </main>
         <!-- main section end -->
+
+        <!-- right section start -->
+        <div class="right">
+            <h1>right</h1>
+        </div>
+        <!-- right cestion end -->
     </div>
 
 
     <!-- footer section start -->
-    <footer class="copyr">
+     <div>
+     <footer class="copyr">
         <div class="container ">
             <div class="row col-12 pt-3 ">
                 <p class="copy-right">Copyright &COPY; 2024 MD-Style Haven SHOP | Develop by - <a href="#"> Malindu </a> </p>
             </div>
         </div>
     </footer>
+     </div>
     <!-- footer section end -->
 </body>
 
