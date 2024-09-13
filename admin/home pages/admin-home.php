123839bb2485b85152c85b29ae2cedd518ced749
Dilakshana-Rupasinghe <153206022+Dilakshana-Rupasinghe@users.noreply.github.com>
2024-09-13 11:45:24 +0530

    admin navigation

---
+++ admin/home pages/admin-home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin dashboard</title>
+</head>
+
+<body>
+    <!-- navigation bar start -->
+    <?php
+        include('includes/admin-navigation.php');
+    ?>
+
+
+    <div class="container">
+        <!-- menu section start -->
+        <div class="menu">
+
+        </div>
+        <!-- menu section end -->
+
+        <!-- main section start -->
+        <div class="main">
+
+        </div>
+        <!-- main section end -->
+    </div>
+
+</body>
+
+</html>
