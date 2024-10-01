c68a7cb33f00d4d77a41aef64d694560a46a7046
Dilakshana-Rupasinghe <153206022+Dilakshana-Rupasinghe@users.noreply.github.com>
2024-10-01 20:22:18 +0530

    staff login PHP & SQL part and session handle

--- admin/home pages/staff-login.php
+++ admin/home pages/staff-login.php
@@ -1,3 +1,64 @@
+<?php
+session_start();
+
+//include database connection
+include('../../database/config.php');
+
+// check if the form is submited or not
+if (isset($_POST['staff-login'])) {
+
+    // add user inputs 
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // check the username and passweord are correct or nor
+
+    //verifi if password store in db in correct username
+    $select_quiry = "SELECT * FROM staff WHERE staff_username = '$username'";
+
+    $result = mysqli_query($con, $select_quiry);
+    $row_count = mysqli_num_rows($result);
+    $row_data = mysqli_fetch_assoc($result);
+    if ($row_count > 0) {
+        // check user input passowrd and DB store passweord are maching or not
+        if ($password == $row_data['staff_pwd']) {
+            if ($row_data['staff_is_active'] == 1) { //check user is active
+
+                // store staff id into session
+                $_SESSION['staffId'] = $row_data['staff_id'];
+                $staff_type_id = ($row_data['fk_staff_type_id']);
+
+                // rederect to the user they relevent home page according to staff type id
+                if ($staff_type_id == 1001) {
+                    header("location:admin-home.php");
+                    exit();
+                } //elseif ($staff_type_id == 1002) {
+                //     header('location:#');
+                //     exit();
+                // }elseif($staff_type_id == 1003){
+                //     header('location:#');
+                //     exit();
+                // }elseif($staff_type_id == 1004){
+                //     header('location:#');
+                //     exit();
+                // }elseif($staff_type_id == 1005){
+                //     header('location:#');
+                //     exit();
+                // }
+            }else{
+                echo "<script> alart('User is Deactive'); </script>" ;
+            }
+        }else{
+            echo "<script>alert('Invalid Password');</script>";
+        }
+    }else{
+        echo "<script>alert('Invalid Credentials');</script>";
+    }
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,8 +66,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Md-style-haven</title>
-     <!-- Bootstrap CSS link -->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Bootstrap CSS link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- Google Material icons CSS link -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="../../css/home.css">
@@ -25,7 +86,7 @@
     ?>
 
 
-<div class="sign-up">
+    <div class="sign-up">
         <div class="sign-up-bg col-md-10 mx-auto" style="background: url('../../images/log\ bg1.jpg') no-repeat; background-size: cover;  background-position: center; border-radius: 20px ">
 
 
@@ -48,8 +109,8 @@
                                 <a href="#" class="ps-3 "> Froget-password?</a>
                             </div>
 
-                            <button type="submit" class="submit btn-dark " name="customer-login">Log-in</button>
-                            
+                            <button type="submit" class="submit btn-dark " name="staff-login">Log-in</button>
+
                         </form>
                     </div>
                 </div>
@@ -58,8 +119,8 @@
     </div>
 
 
-      <!-- footer section start -->
-      <div>
+    <!-- footer section start -->
+    <div>
         <footer class="copyr fixed-bottom">
             <div class="container ">
                 <div class="row col-12 pt-3 ">
@@ -73,3 +134,8 @@
 </body>
 
 </html>
+
+<?php
+// close the databse connection
+mysqli_close($con);
+?>
