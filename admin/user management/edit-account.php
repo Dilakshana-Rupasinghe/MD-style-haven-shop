9c91faf7131eb8a9d52b36d0330c0090ccf12462
Dilakshana-Rupasinghe <153206022+Dilakshana-Rupasinghe@users.noreply.github.com>
2024-10-07 19:28:19 +0530

    Update edit-account.php

--- admin/user management/edit-account.php
+++ admin/user management/edit-account.php
@@ -1,21 +1,82 @@
 <?php
+session_start();
+
+//rederect user to login page if user not login 
+if (!isset($_SESSION['staffId'])) {
+    header('location:../home pages/staff-login.php');
+    exit();
+}
+
 // include the database configaration file
 include('../../database/config.php');
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>edit staff account</title>
+    <!-- Bootstrap CSS link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Google Material icons CSS link -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="../../css/home.css">
+    <link rel="stylesheet" href="../../css/back-home.css">
+    <link rel="stylesheet" href="../../css/back-style.css">
+    <link rel="stylesheet" href="../../css/commen.css">
+
+    <!-- material icons CSS link -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+
 </head>
+
 <body>
-    
+<?php
+    include('../../includes/admin-navigation.php');
+    ?>
+    <div class="container-body">
+        <!-- menu section start -->
+        <aside class="left-menu" style="height: fit-content;">
+            <?php
+            include('../../includes/back-side-nav.php');
+            ?>
+        </aside>
+        <div class="middle-side">
+
+            <!-- main section start -->
+            <main class="mx-4">
+                <!-- BACK button start -->
+                <div class="back-button-container mt-1">
+                    <a href="staff-management.php" class="back-button">Back</a>
+                </div>
+                <!-- BACK button end -->
+                <h1>Staff manaement - edit/update user details</h1>
+            </main>
+        </div>
+    </div>
+
+    <!-- footer section start -->
+    <div>
+        <footer class="copyr fixed-bottom  ">
+            <div class="container ">
+                <div class="row col-12 pt-3 ">
+                    <p class="copy-right">Copyright &COPY; 2024 MD-Style Haven SHOP | Develop by - <a href="#"> Malindu </a> </p>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <!-- footer section end -->
+
+    <!--Bootstrap JS link -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
 </body>
+
 </html>
 
-<?php 
+<?php
 // cloce the database connection
 mysqli_close($con);
 ?>
