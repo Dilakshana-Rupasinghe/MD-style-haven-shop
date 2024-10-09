3cb5b9838e60b7f0708abb6dba6c6d2d702dff7e
Dilakshana-Rupasinghe <153206022+Dilakshana-Rupasinghe@users.noreply.github.com>
2024-10-09 14:59:36 +0530

    changes
    add chart to category

--- admin/inventory/category.php
+++ admin/inventory/category.php
@@ -61,6 +61,13 @@ include('../../database/config.php');
                 <!--  BACK & Register button end -->
                 <h1>Category management</h1>
 
+                <div class="charts">
+                    <div class="chart" style="width: 18rem;">
+                        <h3>Strock by Category</h3>
+                        <canvas id="pie-chart"></canvas>
+
+                    </div>
+                </div>
 
 
             </main>
@@ -83,7 +90,7 @@ include('../../database/config.php');
 
     <!-- Chart JS link -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="../../script/strockchart.js"></script>
+    <script src="../../script/categorychart.js"></script>
 
     <!--Bootstrap JS link -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
