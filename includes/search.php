a500b91a9e00d51b7638dfcd9828d32c46df49d2
Dilakshana-Rupasinghe <153206022+Dilakshana-Rupasinghe@users.noreply.github.com>
2024-10-11 20:09:49 +0530

    Create search.php

---
+++ includes/search.php
@@ -0,0 +1,8 @@
+<div class="container col-7">
+    <form action="#" method="get" class="search d-flex  my-4 ">
+        <input type="text" class="me-3 search-bar fw-semibold form-control py-1" placeholder="Search here"
+            name="searchName" required>
+        <button type="submit" class=" rounded-2 py-1 px-3 fw-semibold" name="search"> Search</button>
+    </form>
+
+</div>
