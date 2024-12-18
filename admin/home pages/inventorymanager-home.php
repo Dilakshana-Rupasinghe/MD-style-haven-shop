c016a32de346c37de45f937791f6c30c5e6e47b1
Dilakshana-Rupasinghe <153206022+Dilakshana-Rupasinghe@users.noreply.github.com>
2024-12-18 20:42:52 +0530

    add commen codes to home pages

---
+++ admin/home pages/inventorymanager-home.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['staffId'])) {
+    header('location:staff-login.php');
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin dashboard</title>
+    <!-- Bootstrap CSS link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Google Material icons CSS link -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="../../css/home.css">
+    <link rel="stylesheet" href="../../css/back-home.css">
+    <link rel="stylesheet" href="../../css/back-style.css">
+    <link rel="stylesheet" href="../../css/commen.css">
+    <link rel="stylesheet" href="../../css/manage-button.css">
+
+    <!-- material icons CSS link -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+
+</head>
+
+<body>
+    <!-- navigation bar start -->
+    <?php
+    include('../../includes/admin-navigation.php');
+    ?>
+
+        <!-- footer section start -->
+        <div>
+        <footer class="copyr fixed-bottom ">
+            <div class="container ">
+                <div class="row col-12 pt-3 ">
+                    <p class="copy-right">Copyright &COPY; 2024 MD-Style Haven SHOP | Develop by - <a href="#"> Malindu </a> </p>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <!-- footer section end -->
+
+    <!-- Chart JS link -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="../../script/chart1.js"></script>
+    <script src="../../script/chart2.js"></script>
+
+
+    <!--Bootstrap JS link -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
