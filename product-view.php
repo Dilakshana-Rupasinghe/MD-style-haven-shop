47d0034f3597adb6e716d0e122f335c6f7368d81
Dilakshana-Rupasinghe <153206022+Dilakshana-Rupasinghe@users.noreply.github.com>
2025-01-20 14:58:28 +0530

    Update product-view.php

--- product-view.php
+++ product-view.php
@@ -89,7 +89,9 @@ if (isset($_POST['feddback'])) {
     <!-- material icons css link -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <link rel="stylesheet" href="css/home-all-style.css">
-    <title>MD-Style Haven shop/online shoping-Home page</title>
+    <link rel="stylesheet" href="css/ratings.css">
+
+    <title>MD-Style Haven shop/online shoping-Product view </title>
 </head>
 
 
