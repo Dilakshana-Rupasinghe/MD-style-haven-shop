45f1bec1257b96a6f4a7357675ec73d33c44d8b7
Dilakshana-Rupasinghe <153206022+Dilakshana-Rupasinghe@users.noreply.github.com>
2025-01-21 12:59:32 +0530

    changes

--- admin/home pages/staff-login.php
+++ admin/home pages/staff-login.php
@@ -73,7 +73,7 @@ if (isset($_POST['staff-login'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- Google Material icons CSS link -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="../../css/home.css">
+    <link rel="stylesheet" href="../../css/home-all-style.css">
     <link rel="stylesheet" href="../../css/back-home-style.css">
     <link rel="stylesheet" href="../../css/login.css">
 
