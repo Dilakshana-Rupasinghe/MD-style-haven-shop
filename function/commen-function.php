ef89d4a08c2cb5bb2215c13951da366fcd5c8127
Dilakshana-Rupasinghe <153206022+Dilakshana-Rupasinghe@users.noreply.github.com>
2025-06-11 00:56:52 +0530

    Update commen-function.php

--- function/commen-function.php
+++ function/commen-function.php
@@ -102,28 +102,27 @@
 
         if (isset($_POST['updateCartItem' . $cart_id])) {
             $updatedCartItemQty = (int)$_POST['cartQty'];
-        
             if ($updatedCartItemQty != $item_qty) {
-              // update quantity in user cart
-              $cartUpdateQuery = "UPDATE cart_item SET item_qty=$updatedCartItemQty WHERE cart_id=$cart_id";
-              if (mysqli_query($con, $cartUpdateQuery)) {
-                // update item stock quantity
-                $updatedItemStockQty = 0;
-                if ($updatedCartItemQty > $item_qty) {
-                  // remove adding cart items from item stock
-                  $updatedItemStockQty = $item_stock_qty - ($updatedCartItemQty - $item_qty);
-                } else {
-                  // add removing item to item stock
-                  $updatedItemStockQty = $item_stock_qty + ($item_qty - $updatedCartItemQty);
+                // update quantity in user cart
+                $cartUpdateQuery = "UPDATE cart_item SET item_qty=$updatedCartItemQty WHERE cart_id=$cart_id";
+                if (mysqli_query($con, $cartUpdateQuery)) {
+                    // update item stock quantity
+                    $updatedItemStockQty = 0;
+                    if ($updatedCartItemQty > $item_qty) {
+                        // remove adding cart items from item stock
+                        $updatedItemStockQty = $item_stock_qty - ($updatedCartItemQty - $item_qty);
+                    } else {
+                        // add removing item to item stock
+                        $updatedItemStockQty = $item_stock_qty + ($item_qty - $updatedCartItemQty);
+                    }
+                    // update item table stock quntity 
+                    $itemUpdateQuery = "UPDATE item SET item_stock_qty=$updatedItemStockQty WHERE item_id=$item_id";
+                    if (mysqli_query($con, $itemUpdateQuery)) {
+                        echo "<script>alert('Updated successfully');</script>";
+                        echo "<script>window.open('cart.php', '_self');</script>";
+                        exit();
+                    }
                 }
-                $itemUpdateQuery = "UPDATE item SET item_stock_qty=$updatedItemStockQty WHERE item_id=$item_id";
-                if (mysqli_query($con, $itemUpdateQuery)) {
-                  echo "<script>alert('Updated successfully');</script>";
-                  echo "<script>window.open('cart.php', '_self');</script>";
-                  exit();
-                }
-              }
             }
-          }
         }
-            
+    }
